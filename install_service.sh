9be064a7d2488ab6c9876178579bc3d46d3bd087
Jing Zhi <t.jingzhi1@gmail.com>
2018-11-27 04:24:08 +0800

    fixed install service

--- install_service.sh
+++ install_service.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+wdir=$( cd $(dirname $BASH_SOURCE[0]) && pwd)
+
 #create service for startup
 if [ ! -f /etc/systemd/system/NaughtyKeyboard.service ]; then
            echo "Injecting startup script..."
