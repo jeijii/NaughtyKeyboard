ecd58283acdf03f36945749ba72798ea5f8b1de3
Jing Zhi <t.jingzhi1@gmail.com>
2019-01-24 21:51:26 +0800

    added server

---
+++ server.py
@@ -0,0 +1,61 @@
+import socket, threading, sys, cmd
+
+bind_ip = '0.0.0.0'
+bind_port = 4444
+
+print("Welcome to Naughty Server")
+print("Waiting for client to et phone home")
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((bind_ip, bind_port))
+server.listen(1)
+
+conn, address = server.accept()
+print("Connected: "+repr(address))
+
+def writeall(sock):
+    while True:
+        data = sock.recv(256)
+        if not data:
+            sys.stdout.write("\r\n*** EOF ***\r\n\r\n")
+            sys.stdout.flush()
+        sys.stdout.write(data)
+        sys.stdout.flush()
+
+
+def launchshell():
+    writer = threading.Thread(target=writeall, args=(conn,))
+    writer.start()
+    try:
+        while True:
+            d = sys.stdin.read(1)
+            if not d:
+                break
+            conn.send(d)
+    except EOFError:
+        pass
+
+
+class shell(cmd.Cmd):
+    prompt = "Naughty> "
+    file = None
+
+    def do_list_scripts(self, arg):
+        'View all the scripts available in the pi'
+        conn.send("ls -l /home/pi/NaughtyKeyboard/dakencoder \r\n")
+        conn.recv(256)
+        conn.recv(256)
+        print(str(conn.recv(256)).rsplit("\n", 2)[0])
+
+    def do_launch_scripts(self, arg):
+        'Execute a ducky script to connected computer. \nExample: launch_script rickroll.txt'
+        print(arg)
+        data = "python /home/pi/NaughtyKeyboard/dakencoder/dakencoder.py /home/pi/NaughtyKeyboard/dakencoder/"+arg+" \r\n"
+        conn.send(data)
+
+    def do_shell(self, arg):
+        'open an interactive shell to the pi'
+        launchshell()
+
+if __name__ == '__main__':
+    shell().cmdloop()
+
